ccfa3103b1d3fda04047384f85e10afdaaeb24b6
Andy Perez <andrewrayp@hotmail.com>
2015-10-05 18:19:15 -0600

    made article page, set peramiters

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  Rails.application.routes.draw do
+
+    resources :articles
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
+ end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
